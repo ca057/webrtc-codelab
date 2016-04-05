88fc5a344074d110081f163450a7d2eff4c2c60e
Christian Ost <christian@christianost.de>
2016-04-06 00:29:42 +0200

    Restructured app and added some functionality

--- index.js
+++ index.js
@@ -4,20 +4,42 @@ var http = require('http').Server(app);
 // new instance of socket.io, passing the http/server object
 var io = require('socket.io')(http);
 
+const simpleChat = (socket) => {
+  if (!socket) {
+    console.warn('No socket was passed, chat could not be set up.');
+    return false;
+  }
+  return {
+    'newClient': () => {
+      console.log('a user connected');
+      socket.broadcast.emit('info', 'Es gibt jemanden Neuen im Chat...');
+    },
+    'clientLeft': () => {
+      console.log('user disconnected');
+      socket.broadcast.emit('info', 'Es gibt eine Person weniger im Chat...');
+    },
+    'newMessage': (msg) => {
+      console.log(`message: ${msg}`);
+      socket.broadcast.emit('chat', msg);
+    }
+  };
+};
+
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  const chat = simpleChat(socket);
+  chat.newClient();
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    chat.clientLeft();
   });
 
   socket.on('chat', (msg) => {
-    console.log(`message: ${msg}`);
-    io.emit('chat', msg);
+    chat.newMessage(msg);
   });
 });
 
--- public/ressources/main.js
+++ public/ressources/main.js
@@ -1,12 +1,26 @@
 $(() => {
   var socket = io();
+
+  var newMessage = () => {
+    $('html, body').animate({scrollTop: $(document).height()}, 500);
+  };
+
   $('form').submit(() => {
-    socket.emit('chat', $('#m').val());
+    const msg = $('#m').val();
+    socket.emit('chat', msg);
+    $('#messages').append($('<li class="own">').text(msg));
+    newMessage();
     $('#m').val('');
     return false;
   });
 
   socket.on('chat', (msg) => {
-    $('#messages').append($('<li>').text(msg));
+    $('#messages').append($('<li class="partner">').text(msg));
+    newMessage();
+  });
+
+  socket.on('info', (info) => {
+    $('#messages').append($('<li class="info">').text(info));
+    newMessage();
   });
 });
